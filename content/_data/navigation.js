55e2b9f2346573c1e401face32a136a41fe2d3f6
minhadona <49408880+minhadona@users.noreply.github.com>
2026-07-13 12:28:30 -0300

    Add navigation data module with links

---
+++ content/_data/navigation.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        title: "Home",
+        url: "/"
+    },
+    {
+        title: "Blog",
+        url: "/blog/"
+    },
+    {
+        title: "Disease",
+        url: "/disease/"
+    },
+    {
+        title: "Links",
+        url: "/links/"
+    },
+    {
+        title: "Pics",
+        url: "/pics/"
+    }
+];
